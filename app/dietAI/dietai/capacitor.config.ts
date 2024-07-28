a159c420512ddc3abb89c372e7edad523e518723
doronsh1 <67556123+doronsh1@users.noreply.github.com>
2024-07-27 20:12:51 -0400

    Delete app/dietAI directory

--- app/dietAI/dietai/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'ionic.dietai',
-  appName: 'dietai',
-  webDir: 'dist'
-};
-
-export default config;
